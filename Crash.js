c47e346444c39500bfd24066740a61c8fd8c4dc5
Eunice Lim <89880114+euniceyujean@users.noreply.github.com>
2023-10-16 22:37:30 -0500

    edits to crash code

--- Crash.js
+++ Crash.js
@@ -9,32 +9,34 @@ const probability_ranges = [
     {"min_time": 50, "max_time": 55, "total_probability": 0.1},  // 50-55: 10% crash
     ];
 
-const earnElement = document.getElementById("earn");
-const numberInput = document.getElementById("number-input");
+// const earnElement = document.getElementById("earn");
+const numberInput = document.getElementById("numberInput");
 const timerElement = document.getElementById("timer");
-const currentTimeElement = document.getElementById("current-time");
 
 let startTime = Date.now();
-let crashTime = Math.random() * 10 * 1000;
+let crashTime = Math.random() *20 *1000;
+let timerInterval;
 
 // Timer Update
 function updateTimer() {
     const currentTime = Date.now();
-    const elapsedTime = (currentTime-startTime)/1000;
+    const elapsedTime = (currentTime - startTime)/1000;
     timerElement.textContent = elapsedTime.toFixed(2);
 
     if (elapsedTime >= crashTime) {
         timerElement.textContent = "Crashed!";
+        
     } else {
         requestAnimationFrame(updateTimer);
     }
 }
    
 // Exponential Timer
-function exponentialTimer(){
+function multiplier(){
     startTime = Date.now();
-    crashTime = Math.random() * 10 * 1000;
-    updateTimer();
+    crashTime = Math.random() *20 *1000;
+    timerInterval = setInterval(updateTimer, 1000);
+    setInterval(updateCurrentTime, 1000);
 }
 
 
@@ -43,11 +45,18 @@ function multiplyNumber(){
     const numberInput = document.getElementById("numberInput").value;
     const parsedNumber = parseFloat(numberInput);
     const currentTimestamp = parseFloat(timerElement.textContent);
-    
-    const earn = parsedNumber * currentTimestamp; // You can change the multiplier as needed
-    document.getElementById("earn").textContent = "You won $" + earn;
-    clearInterval(timerInterval); // Clear the timer interval after multiplying
+    const earn = parsedNumber * currentTimestamp;
 
+    document.getElementById("earn").textContent = "You won $" + earn;
+}
+
+//Disable button
+function disableButton(){
+    const betButton = document.getElementById("bet")
+    const cashoutButton = document.getElementById("cashout")
+
+    betButton.disabled = true;
+    cashoutButton.disabled = true;
 }
 
 
