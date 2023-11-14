7532ea32887e574cc73de8df57dc96a96dd97130
Eunice Lim <89880114+euniceyujean@users.noreply.github.com>
2023-11-14 09:34:52 -0600

    Replaced rage button with Daniel's pic

--- Rage.js
+++ Rage.js
@@ -1,4 +1,4 @@
 function pictureChange(){
-    document.getElementById("rageButton").src="images/chickenpic.png";
-    setTimeout(() => {   document.getElementById("rageButton").src="images/angryemoji-purple.png"; }, 200);
+    document.getElementById("rageButton").src="images/IMG_0001.png";
+    setTimeout(() => {   document.getElementById("rageButton").src="images/IMG_0002.png"; }, 200);
 }
