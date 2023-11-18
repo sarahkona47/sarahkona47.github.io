f5eedc60911f03014fc6f449d50b1c964441c62a
Eunice Lim <89880114+euniceyujean@users.noreply.github.com>
2023-11-17 21:47:15 -0600

    added to popup windows

--- sellBuyPopup.js
+++ sellBuyPopup.js
@@ -4,6 +4,7 @@ function sellPopupWin() {
     popup.classList.toggle("show");
   }
 
+
 // Buy chickens popup window
 function buyPopupWin() {
   var popup = document.getElementById("buyChickenPopup");
