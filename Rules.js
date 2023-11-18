9d49384173925093458373a3d3ea582d82dac12f
Eunice Lim <89880114+euniceyujean@users.noreply.github.com>
2023-11-17 22:36:46 -0600

    added rules popup window

--- Rules.js
+++ Rules.js
@@ -1,3 +1,12 @@
+let popup = document.getElementsByClassName("rules")[0];
+let closeBtn = document.getElementById("rulesClose");
+
 function rulesPopupWin() {
-    
+    popup.style.transform = "translateY(0)";
   }
+
+closeBtn.addEventListener("click", (e) => {
+    popup.style.transform = "translateY(-70vh)";
+});
+
+window.onload = rulesPopupWin();
