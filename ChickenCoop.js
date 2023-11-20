c61ec9e41bce5ebe636197b2db8513386fc719af
Eunice Lim <89880114+euniceyujean@users.noreply.github.com>
2023-11-20 16:38:25 -0600

    edited ChickenCoop
    work in progress

--- ChickenCoop.js
+++ ChickenCoop.js
@@ -1,41 +1,54 @@
-import { currentBalance } from "./Crash";
+import "./Crash";
+import currentBalance from './Crash';
 
-const startingChickens = 1;
-const totalchickens = startingChickens; 
+var totalchickens = 1; 
 const chickenValue = 500;
+const buyInput = parseFloat(document.getElementById("buyChickenNum").toFixed(0));
+const sellInput = parseFloat(document.getElementById("sellChickenNum").toFixed(0));
+const boughtValue = chickenValue*buyInput;
+const soldValue = chickenValue*sellInput;
+
+document.getElementById("sellButton").addEventListener("click", sellChicken());
+document.getElementById("buyButton").addEventListener("click", buyChicken());
 
 function buyChicken() {
-    if (currentBalance > chickenValue){
-        totalchickens = (totalchickens + 1).toFixed(0)
+    if (currentBalance >= chickenValue){
+        totalchickens = (totalchickens + buyInput).toFixed(0)
+        deductBalance(boughtValue);
+        document.getElementsByClassName("buyMessage").textContent = "You bought" + buyInput + " chickens!";
     }
     else{
-        document.getElementsByClassName("navber_buy").textContent = "You don't have enough money!"; 
+        document.getElementsByClassName("buyMessage").textContent = "You don't have enough money!"; 
     }
 }
 
 function sellChicken() {
-    if (totalchickens >=1) {
-        totalchickens = (totalchickens-1).toFixed(0);
-        currentBalance = currentBalance + chickenValue
-    }
+    totalchickens = (totalchickens-sellInput).toFixed(0);
+        addBalance((soldValue));
+        document.getElementsByClassName("buyMessage").textContent = "You sold" + sellInput + " chickens!";
+    // if (totalchickens >= buyInput) {
+    //     totalchickens = (totalchickens-sellInput).toFixed(0);
+    //     addBalance((soldValue));
+    //     document.getElementsByClassName("buyMessage").textContent = "You sold" + sellInput + " chickens!";
+    // }
     
-    else {
-        document.getElementsByClassName("navber_sell").textContent = "You don't have any chickens!"; 
-    }
+    // else {
+    //     document.getElementsByClassName("sellMessage").textContent = "You don't have any chickens!"; 
+    // }
 }
 
 function chickenImage(){
     let img = document.createElement('img');
     img.src = 'images/chicken.png';
     img.style.height = '50px';
-    img.style.width = '50px'; 
-    document.getElementById('chickencoop').appendChild(img);
+    img.style.width = '50px';
 }
 
 function updateChickensOnScreen(){
-    for (let chicken in totalchickens) {
+    for (let i = 0; i < totalchickens; i++) {
         chickenImage();
+        
     }
 }
 
-updateChickensOnScreen();
+// updateChickensOnScreen();
--- Crash.js
+++ Crash.js
@@ -208,6 +208,11 @@ function addToBalance(){
     document.getElementById("myBalance").textContent = currentBalance.toFixed(2);
 }
 
+function addBalance(val){
+    currentBalance = parseFloat(currentBalance+parseFloat(val));
+    document.getElementById("myBalance").textContent = currentBalance.toFixed(2);
+}
+
 // If cashout button is still enabled (the user didn't press it), deducts lost money from bank balance
 function deductFromBalance(){
     const numberInput = document.getElementById("numberInput").value;
@@ -226,6 +231,11 @@ function deductFromBalance(){
             document.getElementById("earn").textContent = "Oh no! You lost $" + parsedNumber.toFixed(2) + "!";
         }
     }
+}
+
+function deductBalance(val){
+    currentBalance = parseFloat(currentBalance-parseFloat(val));
+    document.getElementById("myBalance").textContent = currentBalance.toFixed(2);
 }
 
 // export{currentBalance}
