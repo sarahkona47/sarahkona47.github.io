a134dd6c412d6170c6eb135428a1d3d8ec24942d
Eunice Lim <89880114+euniceyujean@users.noreply.github.com>
2023-12-13 14:57:04 -0600

    Update popUpComponents.js

--- popUpComponents.js
+++ popUpComponents.js
@@ -1,14 +1,21 @@
+// Manages popup components
+
+// Popup when lose
 function showLosePopup() {
   document.getElementById('losepopup').style.display = 'block';
 }
 
+// Closes lose popup
 function closeLosePopup() {
   document.getElementById('losepopup').style.display = 'none';
 }
+
+// Popup when win
 function showWinPopup() {
   document.getElementById('winPopup').style.display = 'block';
 }
 
+// Closes win popup
 function closeWinPopup(){
   document.getElementById('winPopup').style.display = 'none';
 }
